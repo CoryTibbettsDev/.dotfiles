c382749feb1945fff12195903cdfb827116fe149
user <user@example.com>
2021-02-07 00:24:11 -0500

    Updated symlink script with function takes folder as argument

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -10,13 +10,21 @@ for FILE in $HOMEFILES; do
 	DOTFILE=~/.$FILE
 	ln -srf $FILE $DOTFILE && echo $DOTFILE links to $FILE || echo $FILE not linked
 done
+# Move back to starting directory
+cd ..
 
-mkdir ~/.config/awesome/
-cd ../awesome/
-AWESOMEFILES=$(ls)
-for FILE in $AWESOMEFILES; do
-	LINKFILE=~/.config/awesome/$FILE
-	ln -srf $FILE $LINKFILE && echo $LINKFILE links to $FILE || echo $FILE not linked
-done
+linkconfig() {
+	echo linking $1
+	mkdir ~/.config/$1
+	cd $1
+	FOLDERFILES=$(ls)
+	for FILE in $FOLDERFILES; do
+		LINKFILE=~/.config/$1
+		ln -srf $FILE $LINKFILE && echo $LINKFILE links to $FILE || echo $FILE not linked
+	done
+}
+
+linkconfig awesome/
+linkconfig termite/
 
 echo Done
